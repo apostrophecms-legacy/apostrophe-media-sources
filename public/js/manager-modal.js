38fddf9e050e2f6fe3e2d876eb54ac10a1954540
Val <valentin.jeudy@gmail.com>
2020-11-30 14:03:32 +0100

    wip medias source pager

--- public/js/manager-modal.js
+++ public/js/manager-modal.js
@@ -36,42 +36,41 @@ apos.define('apostrophe-images-manager-modal', {
 apos.define('media-source-browser', {
   extend: 'apostrophe-modal',
   source: 'media-source-browser',
-  construct: function (self, options) {
-    // const superAfterShow = self.afterShow;
-    // self.afterShow = function (cb) {
-
-    //   const headerHeight = self.$el.find('.apos-modal-header').outerHeight();
-
-    //   console.log('headerHeight ===> ', headerHeight);
-
-    //   self.resizeContentHeight();
-
-    //   return superAfterShow(cb);
-    // };
-
-    const superBeforeShow = self.beforeShow;
-    self.beforeShow = function(callback) {
+  construct: (self, options) => {
+    // const superBeforeShow = self.beforeShow;
+    self.beforeShow = (done) => {
       const $form = self.$el.find('[data-media-sources-form]');
-
-      const { action } = $form.data();
 
       $form.keypress(({ originalEvent }) => {
         if (originalEvent.charCode === 13) {
-          const formData = self.getFormData();
-
-          $.post(action, formData, function(data) {
-            self.injectResultsLabel(data.results.length, data.total, 1);
-            self.injectResultsList(data.results);
-          });
+          self.requestMediaSource($form, 1);
         }
       });
 
-      return superBeforeShow(callback);
+      done();
     };
 
-    self.getFormData = () => {
-      const $form = self.$el.find('[data-media-sources-form]');
+    self.requestMediaSource = ($form, page) => {
+      const formData = {
+        ...self.getFormData($form),
+        page
+      };
 
+      const { action } = $form.data();
+
+      $.post(action, formData, (data) => {
+
+        self.injectResultsLabel(data.results.length, data.total, page);
+        self.injectResultsList(data.results);
+        self.injectResultsPager({
+          $form,
+          currentPage: page,
+          totalPages: data.totalPages
+        });
+      });
+    };
+
+    self.getFormData = ($form) => {
       const widthValue = $form.find('[data-media-sources-width-value]').val();
       const widthRange = $form.find('[data-media-sources-width-range]').val();
       const heightValue = $form.find('[data-media-sources-height-value]').val();
@@ -111,7 +110,7 @@ apos.define('media-source-browser', {
       const htmlToInject = results.reduce((acc, item) => {
         return `
           ${acc}
-          ${self.getHtmlItem(item)}
+          ${self.getHtmlListItem(item)}
         `;
       }, '');
 
@@ -119,7 +118,7 @@ apos.define('media-source-browser', {
       $items.append(htmlToInject);
     };
 
-    self.getHtmlItem = (item) => {
+    self.getHtmlListItem = (item) => {
       return `
       <div class="apos-manage-grid-piece">
       <div class="apos-manage-grid-image">
@@ -150,6 +149,129 @@ apos.define('media-source-browser', {
     //   <div class="apos-manage-grid-piece-label">{{ piece.title }}</div>
     //   {{ fields.checkbox(data.options.name + '-select') }}
     // </div>
+    };
+
+    self.injectResultsPager = ({
+      $form, currentPage, totalPages
+    }) => {
+      const $pager = self.$el.find('[data-media-sources-pager]');
+
+      const htmlPager = self.getHtmlPager(currentPage, totalPages);
+
+      $pager.empty();
+      $pager.append(htmlPager);
+
+      const pagerLinks = $pager.find('[data-apos-page]');
+
+      pagerLinks.each((index, item) => {
+        const page = $(item).data('apos-page');
+
+        $(item).on('click', () => {
+          self.requestMediaSource($form, page);
+        });
+
+      });
+    };
+
+    self.getHtmlPager = (current, total) => {
+      const maxPages = 6;
+      const pagerSize = (total < maxPages ? total : maxPages);
+
+      // We add two for the gap and the last page
+      const pagerIterator = [ ...Array(pagerSize + 2).keys() ];
+
+      const pagerToInject = pagerIterator.reduce((acc, index) => {
+        const pageNumber = index + 1;
+
+        const gapItem = '<span class="apos-pager-gap">...</span>';
+
+        const getHtmlItem = ({
+          num,
+          isLast,
+          isActive,
+          isFirst
+        }) => {
+          return `<span class="apos-pager-number
+            ${isFirst ? ' apos-first' : ''}
+            ${isLast ? ' apos-last' : ''}
+            ${isActive ? ' apos-active' : ''}
+            ">
+            ${!isActive ? `<a data-apos-page=${num}>${num}</a>` : num}
+          </span>`;
+        };
+
+        const numberToRender = () => {
+          switch (pageNumber) {
+            case 2:
+              return current - 2;
+            case 3:
+              return current - 1;
+            case 4:
+              return current;
+            case 5:
+              return current + 1;
+            case 6:
+              return current + 2;
+          }
+        };
+
+        if (current > 4) {
+          const num = numberToRender();
+
+          if (num) {
+            return `${acc}${pageNumber === 2 ? gapItem : ''}${getHtmlItem({
+              num,
+              isActive: num === current
+            })}`;
+          }
+
+          // if (pageNumber === 2) {
+          //   return `${acc}${gapItem}${getHtmlItem({ num: current - 2 })}`;
+          // }
+          // if (pageNumber === 3) {
+          //   return `${acc}${getHtmlItem({ num: current - 1 })}`;
+          // }
+          // if (pageNumber === 4) {
+          //   return `${acc}${getHtmlItem({
+          //     num: current,
+          //     isActive: true
+          //   })}`;
+          // }
+          // if (pageNumber === 5) {
+          //   return `${acc}${getHtmlItem({ num: current + 1 })}`;
+          // }
+          // if (pageNumber === 6) {
+          //   return `${acc}${getHtmlItem({ num: current + 2 })}`;
+          // }
+        }
+
+        if (pageNumber === pagerSize + 1) {
+          return `${acc}${gapItem}`;
+        }
+        if (pageNumber === pagerSize + 2) {
+          return `${acc}${getHtmlItem({
+            num: total,
+            isLast: true
+          })}`;
+        }
+
+        // const firstClass = pageNumber === 1 ? ' apos-first' : '';
+        // const activeClass = pageNumber === current ? ' apos-active' : '';
+
+        // const item = `<span class="apos-pager-number${firstClass}${activeClass}">
+        //   ${!activeClass ? `<a data-apos-page=${pageNumber}>${pageNumber}</a>` : pageNumber}
+        // </span>`;
+
+        const item = getHtmlItem({
+          num: pageNumber,
+          isActive: pageNumber === current,
+          isFirst: pageNumber === 1
+        });
+
+        return `${acc}${item}`;
+      }, '');
+
+      return pagerToInject;
     };
   }
 });

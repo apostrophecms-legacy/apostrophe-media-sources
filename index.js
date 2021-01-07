ce526f23ac04063823fdef465391b7b41ef86255
Val <valentin.jeudy@gmail.com>
2021-01-07 15:21:50 +0100

    add all cursors with define

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ module.exports = {
     ]
   },
   construct: function(self, options) {
+    self.apos.define('apostrophe-images-cursor', require('./lib/improveCursor.js'));
+
     self.on('apostrophe:modulesReady', 'getAllImagesConnectorsModules', () => {
       // Find all images connectors defined in app configuration
       self.connectors = Object.values(self.apos.modules)

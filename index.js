cf11145068f996d6e25e711633d48459b605d1df
Val <valentin.jeudy@gmail.com>
2021-03-08 19:01:39 +0100

    moves lost code from routes

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ module.exports = {
   },
   construct: (self, options) => {
     require('./lib/api.js')(self, options);
+
+    self.on('apostrophe:modulesReady', 'getAllImagesConnectorsModules', () => {
+      self.buildFilters();
+
+      self.on('apostrophe-pages:beforeSend', 'sendConnectorsToBrowser', (req) => {
+        req.browserCall('apos.mediaSourceConnectors=?', JSON.stringify(self.connectors));
+      });
+    });
+
     require('./lib/routes.js')(self, options);
     require('./lib/assets.js')(self);
     require('./lib/improveCursor.js')(self);
